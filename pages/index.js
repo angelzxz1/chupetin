c26679134039de73acb643082a809ee1c0723988
angelzxz1 <angel.zuna@icloud.com>
2022-10-10 03:00:32 -0500

    Removed the modal and imported from components

--- pages/index.js
+++ pages/index.js
@@ -5,49 +5,19 @@ import {
   Button,
   Grid,
   useColorModeValue,
+  IconButton,
+  useDisclosure,
+  ScaleFade,
 } from "@chakra-ui/react";
-import { useState } from "react";
-const QuestionModal = ({ showModal, setShowModal }) => {
-  return (
-    <Flex
-      w="100%"
-      h="r.v.14"
-      position="fixed"
-      top={14}
-      left="0"
-      zIndex="100"
-      justifyContent="center"
-      alignItems="center"
-      display={showModal ? "flex" : "none"}
-      bg="rgba(0,0,0,0.3)"
-    >
-      <Grid
-        w="500px"
-        h="500px"
-        borderRadius="10px"
-        boxShadow="0 0 10px 0 rgba(0,0,0,0.2)"
-        p="20px"
-        bg={useColorModeValue("white", "gray.800")}
-      >
-        <Heading mb="20px">Question</Heading>
-
-        <Button
-          onClick={() => {
-            setShowModal(false);
-          }}
-          colorScheme="green"
-        >
-          Yes
-        </Button>
-        <Button>No</Button>
-      </Grid>
-    </Flex>
-  );
-};
+import QuestionModal from "../components/modal";
+import { CloseIcon } from "@chakra-ui/icons";
+import { useState, useEffect } from "react";
+import game from "../lib/game";
 
 const Main = () => {
   const [fullH, setFullH] = useState("calc(100vh - 3.5rem)");
   const [showModal, setShowModal] = useState(false);
+  const { isOpen, onToggle } = useDisclosure();
   return (
     <Flex fontFamily="roboto" direction="column">
       <Flex
@@ -55,18 +25,31 @@ const Main = () => {
         h={fullH}
         justify="center"
         align="center"
-        transition="all 0.5s ease-in-out"
-        bg="purple.200"
+        transition="height 0.5s ease-in-out"
+        // bg="purple.200"
       >
         <Button
           onClick={() => {
             setFullH("20");
-            setShowModal(true);
+            setTimeout(() => {
+              setShowModal(true);
+              onToggle();
+            }, 500);
           }}
         >
           Start
         </Button>
-        <QuestionModal showModal={showModal} setShowModal={setShowModal} />
+        {showModal ? (
+          <ScaleFade initialScale={1} in={isOpen}>
+            <QuestionModal
+              onToggle={onToggle}
+              setShowModal={setShowModal}
+              setFullH={setFullH}
+            />
+          </ScaleFade>
+        ) : (
+          <></>
+        )}
       </Flex>
     </Flex>
   );
