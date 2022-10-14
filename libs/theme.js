d09bafba51cc1b50593432203b05c43f4a0742b3
angelzxz1 <angel.zuna@icloud.com>
2022-10-14 17:33:01 -0500

    added a gradient background

--- libs/theme.js
+++ libs/theme.js
@@ -15,7 +15,13 @@ const theme = extendTheme({
     global: (props) => ({
       fontFamily: "'M PLUS Rounded 1c', sans-serif",
       body: {
-        bg: mode("LemonLime.Mint", "LemonLime.Navy")(props),
+        bg: mode(
+          "linear-gradient(132deg, rgba(104,0,255,1) 0%, rgba(82,244,255,1) 50%, rgba(76,255,0,1) 100%)",
+          "linear-gradient(132deg, #1e2137ff 0%, #261440ff 100%)"
+        )(props),
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        width: "100vw",
       },
     }),
   },
